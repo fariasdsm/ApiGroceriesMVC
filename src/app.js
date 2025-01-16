656eee12cb1d205579f0cbf66080a66d00ba2091
fariasdsm <230389@utxicotepec.edu.mx>
2025-01-16 16:01:51 -0600

    Primeros cambios del proyecto

--- src/app.js
+++ src/app.js
@@ -5,5 +5,10 @@ const app = express();
 //Settings
 app.set('port', process.env.PORT || 3000);
 //Middlewares
-
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 //Routes
+
+
+export default app;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
 //Aqui arrancamos el servidor
+import app from './app.js';
+app.listen(app.get('port'),()=>console.log('Server on port', app.get('port')));
